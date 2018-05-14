e434584c4e1cbf13e8ab68bf50353e1e13b880a5
github username <github email address>
2018-05-14 17:57:39 -0500

    sketching out a binary tree checker

--- binary_search_tree/binary_search_tree.rb
+++ binary_search_tree/binary_search_tree.rb
@@ -3,8 +3,8 @@ class BinaryTreeNode
   attr_accessor :value
   attr_reader :left, :right
 
-  def initialize(value)
-    @value = value
+  def initialize(root)
+    @root = root
     @left  = nil
     @right = nil
   end
@@ -17,5 +17,25 @@ class BinaryTreeNode
   def insert_right(value)
     @right = BinaryTreeNode.new(value)
     return @right
+  end
+
+  def binary_search_tree_checker(height)
+    #is this valid
+      #are all the nodes to the left less than the root node
+      #are all the nodes to the right more than the root node
+    #start at root node
+    #current_node = @root
+    #check left side of tree
+    current_node = @root
+    counter = -1
+    until counter == height
+    if current_node > @left
+      current_node = @left
+      counter += 1
+      #loop again
+    elsif current_node < @left || current_node == @left
+      return false
+    end
+
   end
 end

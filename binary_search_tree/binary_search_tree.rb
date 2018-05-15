420ce6d2eef93f7ebf837e10abc4a3585262beff
github username <github email address>
2018-05-15 17:55:58 -0500

    binary search tree

--- binary_search_tree/binary_search_tree.rb
+++ binary_search_tree/binary_search_tree.rb
@@ -25,31 +25,31 @@ class BinaryTreeNode #note that this just determines the relationship between
       #are all the nodes to the left less than the root node
       #are all the nodes to the right more than the root node
     #start at root node
-    #current_node = @root
+    #don't like using @root like this, but it
     #check left side of tree
-    counter = -1
-    until counter == height
-      current_node = @root
-      if current_node > @left
-        current_node = @left
-        counter += 1
-      #loop again
-      elsif current_node < @left || current_node == @left
-        return false
-      end
-    end
-    #right side
-    counter = -1
-    until counter == height
-      current_node = @root
-      if current_node < @right
-        current_node = @right
-        counter += 1
-        #loop again
-      elsif current_node < @right || current_node == @right
-        return false
-      end
-    end
 
+     # if the current node does not exist--this is at the end of the tree
+    #returns true if we make it through the tree and bottom out
+    root_node = @root
+    if @root == nil
+      return true
+    elsif @left != nil #if there is a left node and a root node
+      if @root > @left #checks a node against it's children
+        @root = @left #current
+        binary_search_tree_checker
+      else
+        binary_search_tree_checker
+      end
+    elsif @right != nil  #if there is a right node
+      if @root < @right #if root is less than the node we're checking
+        @root = @right #now
+        binary_search_tree_checker
+      else
+        binary_search_tree_checker
+      end
+    else
+      return false
+    end
   end
+
 end

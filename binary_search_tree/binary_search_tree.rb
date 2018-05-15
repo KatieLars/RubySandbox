65f187f2813692f9f657c368faf3499093f7984e
github username <github email address>
2018-05-15 18:16:48 -0500

    back to 0

--- binary_search_tree/binary_search_tree.rb
+++ binary_search_tree/binary_search_tree.rb
@@ -20,36 +20,16 @@ class BinaryTreeNode #note that this just determines the relationship between
     return @right
   end
 
-  def binary_search_tree_checker(height)
+  def binary_search_tree_checker
     #is this valid
       #are all the nodes to the left less than the root node
       #are all the nodes to the right more than the root node
-    #start at root node
-    #don't like using @root like this, but it
-    #check left side of tree
+    #TOMORROW: steps for making recursive
+      #1. make it non-recursive
+      #2. Exit strategy (what will get us out of the loop)
+      #3. recursive method call
+end
 
-     # if the current node does not exist--this is at the end of the tree
-    #returns true if we make it through the tree and bottom out
-    root_node = @root
-    if @root == nil
-      return true
-    elsif @left != nil #if there is a left node and a root node
-      if @root > @left #checks a node against it's children
-        @root = @left #current
-        binary_search_tree_checker
-      else
-        binary_search_tree_checker
-      end
-    elsif @right != nil  #if there is a right node
-      if @root < @right #if root is less than the node we're checking
-        @root = @right #now
-        binary_search_tree_checker
-      else
-        binary_search_tree_checker
-      end
-    else
-      return false
-    end
-  end
+
 
 end

c94bab40eca7d81ffa2b14fb92c93ad4441546f3
github username <github email address>
2018-05-15 19:14:13 -0500

    adding arrays

--- binary_search_tree/binary_search_tree.rb
+++ binary_search_tree/binary_search_tree.rb
@@ -21,45 +21,23 @@ class BinaryTreeNode #note that this just determines the relationship between
   end
 
   def binary_search_tree_checker
-    #iteration:
-    # 1. Compare the left and right child nodes to the root node
-    #   --our current node is the root node
-    #   --if the left child node is smaller, we start
-    #     checking the left branch
-    #   --if there is no left branch && the right node is greater than
-    #     the root node, we start checking the right side
-    #   --if the left node is greater than the root node
-    #     return false
-    #   --if the right node is smaller than the root node,
-    #     return false
-    # 2. Checking the left side:
-    #   --our current node is the left node
-    #   --Goes through the same steps as no. 1, but checks the left
-    #     and right nodes also against the value of the root node
-    #   --if the left or right node is greater than the root node,
-    #     return false
-    #   --if the current node equals nil
-    #     return true
-    #   3. Checking the right side:
-    #     --our current node is the right node
-    #     --repeat no. 2
     #is this valid
       #are all the nodes to the left less than the root node
       #are all the nodes to the right more than the root node
-    #TOMORROW: steps for making recursive
-      #1. make it non-recursive
-      #2. Exit strategy (what will get us out of the loop)
-      #3. recursive method call
-      current_node = @root
-      check_nodes(current_node)
-end
+    current_node = @root
+    left_nodes = []
+    right_nodes = []
+    check_nodes(current_node, left_nodes, right_notes)
+  end
 
-def check_nodes(current_node)
-  if @left != nil & current_node > @left #if there is a left node if current node is greater than node on left
+def check_nodes(current_node, left_nodes, right_nodes)
+  if @left != nil && current_node > @left #if there is a left node if current node is greater than node on left
     current_node = @left #current_node is left node
+    left_nodes << current_node
     check_nodes(current_node)
-  elsif current_node < @right #right side of tree
+  elsif @right != nil && current_node < @right #right side of tree
     current_node = @right #current_node is right node
+    right_nodes << current_node
     check_nodes(current_node)
   elsif @left == nil && @right == nil
     current_node
